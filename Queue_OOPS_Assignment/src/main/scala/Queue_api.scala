4d4c9fb1dd4245b1b0b91cf2e523df3881eac376
NikhilVarghese21 <76730221+NikhilVarghese21@users.noreply.github.com>
2021-03-15 20:43:31 +0530

    Update Queue_api.scala

--- Queue_OOPS_Assignment/src/main/scala/Queue_api.scala
+++ Queue_OOPS_Assignment/src/main/scala/Queue_api.scala
@@ -7,11 +7,13 @@ package com.knoldus.doubleQueue {
   class DoubleQueue extends Queue {
     //Inserts elements in Queue after doubling the elements.
     def enqueue(element: Int): String = {
+      //Inserting elements when Queue is Empty.
       if (front == -1 && rear == -1) {
         front = front + 1
         rear = rear + 1
         list = list ::: List(2 * element)
       }
+      //Inserting element when Queue is not Empty.
       else {
         rear = rear + 1
         list = list ::: List(2 * element)
@@ -28,11 +30,13 @@ package com.knoldus.squareQueue {
   class SquareQueue extends Queue {
     //Inserts elements in Queue after squaring the elements.
     def enqueue(element: Int): String = {
+      //Inserting elements when Queue is Empty.
       if (front == -1 && rear == -1) {
         front = front + 1
         rear = rear + 1
         list = list ::: List(element * element)
       }
+       //Inserting element when Queue is not Empty.
       else {
         rear = rear + 1
         list = list ::: List(element * element)
